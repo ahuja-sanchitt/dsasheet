816987d58c3ededf05527b6d4bce3a9798de94d7
Sanchit Ahuja <91591902+ahuja-sanchitt@users.noreply.github.com>
2023-06-06 22:46:24 +0530

    MergeIntervals

---
+++ 8.MergeIntervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        
+    vector<vector<int>> ans;
+    vector<int>temp;
+    sort(intervals.begin(),intervals.end());
+    temp=intervals[0];
+    for(auto it:intervals){
+        if(it[0]<=temp[1]){
+            temp[1]=max(it[1],temp[1]);
+        }
+        else{
+            ans.push_back(temp);
+            temp=it;
+        }
+    }
+    ans.push_back(temp);
+    return ans;
+    }
+};
