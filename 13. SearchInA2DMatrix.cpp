614c6d1ffb86f1d85d68a82d4068094c08b7a6d0
Sanchit Ahuja <91591902+ahuja-sanchitt@users.noreply.github.com>
2023-06-07 23:11:50 +0530

    SearchInA2DMatrix

---
+++ 13. SearchInA2DMatrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+     
+    int rows=mat.size();
+    int columns=mat[0].size();
+
+    int i=0; int j=columns-1;
+
+    while(i<rows && j>=0){
+        if(mat[i][j]==target){
+            return true;
+        }
+
+        if(mat[i][j]>target){
+            j--;
+        }
+        else{
+            i++;
+        }
+    }
+
+    
+    return false;   
+    }
+};
